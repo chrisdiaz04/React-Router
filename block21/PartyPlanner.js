ff903f200af0c88e3129bc0a7b37cab89be9e9bc
chrisdiaz04 <diazchris27586@gmail.com>
2024-05-20 22:55:51 -0500

    Remove unwanted directories from Git tracking

--- block21/PartyPlanner.js
+++
@@ -1,6 +0,0 @@
-fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2402-ftb-mt-web-pt/recipes')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data); // Log data to the console or use this data in your state
-    })
-    .catch(error => console.error('Error:', error));
